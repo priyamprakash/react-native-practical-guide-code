abb4a8dedaebac006a817f8f0d9e393cec8247b2
Priyam Prakash <65361533+priyamprakash@users.noreply.github.com>
2023-10-28 12:42:04 +0530

    Update App.js

--- code/01-first-app/App.js
+++ code/01-first-app/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello-World!!!!</Text>
+      <Text>Priyam First App on Git using React-Native and Expo Go !!</Text>
       <StatusBar style="auto" />
     </View>
   );
